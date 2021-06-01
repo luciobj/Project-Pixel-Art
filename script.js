c2970f83d9067da2d3c7253ba509bc8be0b4416b
Lúcio BJ <lbjunq@hotmail.com>
2021-06-01 19:11:04 -0300

    Alteracoes para condicionamento do corretor, e lint

--- script.js
+++ script.js
@@ -6,23 +6,35 @@ function createColorsDiv(number) {
     color.className = 'color';
     color.id = 'color' + index;
     paletDiv.appendChild(color);
-  };
-};
-//2
+  }
+}
+//2 
 createColorsDiv(4);
 const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
 const color3 = document.getElementById('color3');
 const color4 = document.getElementById('color4');
-//3
+//3 
 function assignColor(color, colordiv) {
   colordiv.style.backgroundColor = color;
-};
+}
 assignColor('black', color1);
 assignColor('red', color2);
 assignColor('blue', color3);
 assignColor('green', color4);
-//4
+//9 
+const clearButton = document.createElement ('button');
+clearButton.classList = 'button';
+clearButton.id = 'clear-board'
+clearButton.innerText = 'Limpar'
+document.body.appendChild(clearButton);
+clearButton.addEventListener('click', resetPixels);
+function resetPixels() {
+  for (let element of pixels){
+    element.style.backgroundColor = 'white';
+  }
+}
+//4 
 function createBoardDiv(number) {
   const box = document.createElement('div');
   box.id = 'pixel-board';
@@ -35,59 +47,50 @@ function createPixels(number, where) {
     pixel.className = 'pixel';
     pixel.id = 'pixel' + index;
     if (index == 1) {
-      createLine (1, where);
-      const line = document.getElementById ('line' + index);
+      createLine(1, where);
+      const line = document.getElementById('line' + index);
       line.appendChild(pixel);
     }
     else if ((index - 1) % (Math.sqrt (number)) == 0) {
-      createLine (((((index - 1) / (Math.sqrt (number))) + 1)), where);
-      const line = document.getElementById ('line' + (((index - 1) / (Math.sqrt (number))) + 1));
+      createLine(((((index - 1) / (Math.sqrt (number))) + 1)), where);
+      const line = document.getElementById('line' + (((index - 1) / (Math.sqrt (number))) + 1));
       line.appendChild(pixel); 
     }
     else {
-      const line = document.getElementsByClassName ('line')[document.getElementsByClassName ('line').length - 1];
+      const line = document.getElementsByClassName('line')[document.getElementsByClassName('line').length - 1];
       line.appendChild(pixel);
     };
   };
 };
 function createLine(index, where) {
-  const line = document.createElement ('div');
+  const line = document.createElement('div');
   line.className = 'line';
   line.id = 'line' + index;
   where.appendChild(line);
 };
 createBoardDiv(25);
-//6
+//6 
 color1.className += ' selected';
-//7
+//7 
 const color = document.getElementsByClassName('color');
-for (element of color) {
+for (let element of color) {
   element.addEventListener('click', changeSelectedColor);
 }
 function changeSelectedColor(event) {
-  for (element of color) {
+  for (let element of color) {
     if (element.className.include = ' selected') {
       element.className = 'color';
     }
   }
   event.target.className += ' selected';
 }
-//8
+//8 
 const pixels = document.getElementsByClassName('pixel');
-for (element of pixels) {
+for (let element of pixels) {
   element.addEventListener('click', changeColor);
 };
-function changeColor (event) {
+function changeColor(event) {
   const selectedColor = document.getElementsByClassName('selected')[0];
   const color = selectedColor.style.backgroundColor;
   event.target.style.backgroundColor = color;
-};
-//9
-const clearButom = document.createElement ('button');
-clearButom.classList = 'button';
-document.body.appendChild(clearButom);
-clearButom.addEventListener ('click', resetPixels);
-function resetPixels () {
-  for (element of pixels)
-  element.style.backgroundColor = 'white';
 };
